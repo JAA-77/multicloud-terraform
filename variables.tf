442e617ed533dd7522dec3d01a45de86e6a773eb
JAA-77 <132390901+JAA-77@users.noreply.github.com>
2025-03-12 16:12:51 +0100

    .

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ data "local_file" "mcd_api_key" {
 
 variable "ciscomcd_api_key_file" {
   type = string  
-  content = data.local_file.mcd_api_key.content
+  default = data.local_file.mcd_api_key.content
 }
 
 variable "aws_account_name" {
