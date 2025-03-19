dd19fe1a1deb0c8dd4c14ed6d533166d21bd18b7
JAA-77 <132390901+JAA-77@users.noreply.github.com>
2025-03-19 12:45:27 +0100

    .

--- policy_ruleset.tf
+++ policy_ruleset.tf
@@ -8,3 +8,22 @@ resource "ciscomcd_policy_rule_set" "egressew_policy" {
   name = var.ciscomcd_egress_policy_rule_set_name
 }
 
+
+
+/* Exportado de AWS CloudShell
+
+  # ciscomcd_policy_rule_set.egressew_policy will be created
+  + resource "ciscomcd_policy_rule_set" "egressew_policy" {
+      + id          = (known after apply)
+      + name        = "mcd-egress-ruleset-allow-all"
+      + rule_set_id = (known after apply)
+    }
+
+  # ciscomcd_policy_rule_set.ingress_policy will be created
+  + resource "ciscomcd_policy_rule_set" "ingress_policy" {
+      + id          = (known after apply)
+      + name        = "mcd-ingress-ruleset-allow-all"
+      + rule_set_id = (known after apply)
+    }
+
+*/
