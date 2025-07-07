653c6184820d590ce6263000adfb309f7ef294ce
Jordan <jrdn@gmx.es>
2025-07-07 20:17:35 +0200

    .

--- spoke_vpc.tf
+++ spoke_vpc.tf
@@ -2,13 +2,13 @@
 resource "ciscomcd_spoke_vpc" "ciscomcd_spoke_defender" {
   service_vpc_id                     = ciscomcd_service_vpc.aws_service_vpc.id
   spoke_vpc_id                       = aws_vpc.defender_vpc.id
-  transit_gateway_attachment_subnets = aws_subnet.defender_subnet_pub.vpc_id
+  transit_gateway_attachment_subnets = [aws_subnet.defender_subnet_pub.vpc_id]
 }
 
 resource "ciscomcd_spoke_vpc" "ciscomcd_spoke_attacker" {
   service_vpc_id                     = ciscomcd_service_vpc.aws_service_vpc.id
   spoke_vpc_id                       = aws_vpc.attacker_vpc.id
-  transit_gateway_attachment_subnets = aws_subnet.attacker_subnet_pub.vpc_id
+  transit_gateway_attachment_subnets = [aws_subnet.attacker_subnet_pub.vpc_id]
 }
 
 
